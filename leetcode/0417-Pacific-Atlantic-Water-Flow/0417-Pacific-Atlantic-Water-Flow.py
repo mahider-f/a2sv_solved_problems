28d43e7d01c4ea7f615ab5f463d6357ccbe9fd20
mahider-f <mahiderfeleke1@gmail.com>
2026-04-21 21:47:32 +0300

    Add solution for Pacific Atlantic Water Flow

---
+++ leetcode/0417-Pacific-Atlantic-Water-Flow/0417-Pacific-Atlantic-Water-Flow.py
@@ -0,0 +1,35 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        pacific = set()
+        atlantic = set()
+        res = []
+        
+
+        row, col = len(heights), len(heights[0])
+
+        def dfs(r,c,visited,h):
+            if ((r,c) in visited or r<0 or c<0 or r >= row or c >= col or heights[r][c] < h):
+                return
+
+            visited.add((r,c))
+            dfs(r-1,c,visited,heights[r][c])
+            dfs(r,c-1,visited,heights[r][c])
+            dfs(r+1,c,visited,heights[r][c])
+            dfs(r,c+1,visited,heights[r][c])
+
+        for c in range(col):
+            dfs(0,c,pacific,heights[0][c])
+            dfs(row-1,c,atlantic,heights[row-1][c])
+
+        for r in range(row):
+            dfs(r,0,pacific,heights[r][0])
+            dfs(r,col-1,atlantic,heights[r][col -1])
+
+        
+        for i in range(row):
+            for j in range(col):
+                if (i,j) in pacific and (i,j) in atlantic:
+                    res.append([i,j])
+                
+
+        return res
