45409e1c3f2d3c3d68a35a5e2c984b1dfbfb7dd6
mahider-f <mahiderfeleke1@gmail.com>
2026-05-30 02:06:30 +0300

    Add solution for The Skyline Problem

---
+++ leetcode/0218-the-skyline-problem/0218-the-skyline-problem.py
@@ -0,0 +1,22 @@
+class Solution:
+    def getSkyline(self, buildings):
+        
+        points = []
+        for left, right, height in buildings:
+            points.append((left, -height))  
+            points.append((right, height))  
+        points.sort()
+        heights = [0]  
+        skyline = [(0, 0)]  
+
+        for x, h in points:
+            if h < 0:
+                heapq.heappush(heights, h)  
+            else:
+                heights.remove(-h)  
+                heapq.heapify(heights)  
+            max_height = -heights[0]
+            if max_height != skyline[-1][1]:
+                skyline.append((x, max_height))
+
+        return skyline[1:]  
