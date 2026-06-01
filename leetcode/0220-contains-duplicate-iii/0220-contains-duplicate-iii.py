009615d1b922afd7e493b4dc60c2bd423d8fe41f
mahider-f <mahiderfeleke1@gmail.com>
2026-06-02 01:47:27 +0300

    Add solution for Contains Duplicate III

--- leetcode/0220-contains-duplicate-iii/0220-contains-duplicate-iii.py
+++ leetcode/0220-contains-duplicate-iii/0220-contains-duplicate-iii.py
@@ -1,11 +1,30 @@
 class Solution:
     def containsNearbyAlmostDuplicate(self, nums: List[int], indexDiff: int, valueDiff: int) ->bool:
-        s = SortedSet()
-        for i, v in enumerate(nums):
-            j = s.bisect_left(v - valueDiff)
-            if j < len(s) and s[j] <= v + valueDiff:
+        if valueDiff < 0:
+            return False
+
+        w = valueDiff + 1
+        buckets = {}
+
+        for i, num in enumerate(nums):
+            b = num // w
+
+            # same bucket
+            if b in buckets:
                 return True
-            s.add(v)
+
+            # left bucket
+            if b - 1 in buckets and abs(num - buckets[b - 1]) <= valueDiff:
+                return True
+
+            # right bucket
+            if b + 1 in buckets and abs(num - buckets[b + 1]) <= valueDiff:
+                return True
+
+            buckets[b] = num
+
             if i >= indexDiff:
-                s.remove(nums[i - indexDiff])
+                old = nums[i - indexDiff]
+                del buckets[old // w]
+
         return False
