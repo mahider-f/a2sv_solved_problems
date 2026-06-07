8a09838eaadff1e2167abc48e1a50755af296f94
mahider-f <mahiderfeleke1@gmail.com>
2026-06-07 20:46:09 +0300

    Add solution for Image Smoother

---
+++ leetcode/0661-image-smoother/0661-image-smoother.py
@@ -0,0 +1,21 @@
+class Solution:
+    def imageSmoother(self, img: list[list[int]]) -> list[list[int]]:
+        rows, cols = len(img), len(img[0])
+        res = [[0] * cols for _ in range(rows)]
+        
+        for r in range(rows):
+            for c in range(cols):
+                total_sum = 0
+                count = 0
+                
+               
+                for i in range(r - 1, r + 2):
+                    for j in range(c - 1, c + 2):
+                    
+                        if 0 <= i < rows and 0 <= j < cols:
+                            total_sum += img[i][j]
+                            count += 1
+                
+                res[r][c] = total_sum // count
+                
+        return res
